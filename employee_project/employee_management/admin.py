5b17b25792fdaf0c5295846ee254511d1fbfaeda
Shagun <shagunpatel9903@gmail.com>
2024-01-16 12:45:34 +0530

    admin panel change

--- employee_project/employee_management/admin.py
+++ employee_project/employee_management/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from employee_management.models import Employee,Position,Department
+from employee_management.models import Employee,Position,Department,Permission
 # Register your models here.
+class EmpAdmin(admin.ModelAdmin):
+    fields=['Position_Name','Position_Id','Description']
+    search_fields=['Position_Name','Position_Id','Description']
+    list_filter=['Position_Name','Position_Id']
+    list_display=['Position_Id','Position_Name']
+    list_editable=['Position_Name']
 admin.site.register(Employee)
-admin.site.register(Position)
+# admin.site.register(Runner)
+admin.site.register(Position,EmpAdmin)
 admin.site.register(Department)
+admin.site.register(Permission)
+
+# admin.site.register(Pizza)
+# admin.site.register(Topping)
+
--- employee_project/employee_management/models.py
+++ employee_project/employee_management/models.py
@@ -15,16 +15,23 @@ class Position(models.Model):
     def __str__(self):
         return self.Position_Name
 
+class Permission(models.Model):
+    permission_name=models.CharField(max_length=203)
+    def __str__(self):
+        return self.permission_name
 class Employee(models.Model):
+    gender_choices=[("F","Female"),("M","Male"),("O","Others")]
     Employee_Id=models.IntegerField(primary_key=True)
     First_Name=models.CharField(max_length=256)
     Last_Name=models.CharField(max_length=256)
+    Gender=models.CharField(max_length=1,choices=gender_choices)
     Salary=models.PositiveIntegerField()
     Department=models.ForeignKey(Department,on_delete=models.CASCADE)
     Position=models.ForeignKey(Position,on_delete=models.CASCADE)
     Location=models.CharField(max_length=256)
     Phone_No=models.BigIntegerField()
     Date_Of_Joining=models.DateField()
+    permission=models.ManyToManyField(Permission)
 
     def __str__(self):
         return self.First_Name
@@ -32,6 +39,21 @@ class Employee(models.Model):
 
     def get_absolute_url(self):
         return reverse("emp:view")
+    
+# class Runner(models.Model):
+#     MedalType = models.TextChoices("MedalType", "GOLD SILVER BRONZE")
+#     name = models.CharField(max_length=60)
+#     medal = models.CharField(blank=True, choices=MedalType, max_length=10)
+
+# class Topping(models.Model):
+#     topping_name=models.CharField(max_length=230)
+#     def __str__(self):
+#         return self.topping_name
+
+
+# class Pizza(models.Model):
+    # ...
+    # toppings = models.ManyToManyField(Topping)
     
 
 
